c45cffe0afe781a4bde995b90c7545bfdec0164b
George Patterson <g-patt@outlook.com>
2022-06-02 04:49:33 +1000

    extracted example-next code from web3-react

---
+++ components/Card.tsx
@@ -0,0 +1,18 @@
+export function Card({ children }: { children: JSX.Element | JSX.Element[] }) {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        width: '20rem',
+        padding: '1rem',
+        margin: '1rem',
+        overflow: 'auto',
+        border: '1px solid'
+      }}
+    >
+      {children}
+    </div>
+  )
+}
